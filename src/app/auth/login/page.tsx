d9dd44404e77b41750c81c5dc8518f34ffbe3741
CarlosSalega <carlosalfredosalega@gmail.com>
2026-03-15 10:16:08 -0300

    chore: initial commit 🎉

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Login",
+};
+
+export default function LoginPage() {
+	return (
+		<>
+			<h1>Login</h1>
+		</>
+	);
+}
---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Register",
+};
+
+export default function RegisterPage() {
+	return (
+		<>
+			<h1>Register</h1>
+		</>
+	);
+}
