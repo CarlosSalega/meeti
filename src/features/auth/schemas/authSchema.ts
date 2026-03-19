7fa11b633d024b2c0f747cb0c9ab9a9b28aa7e1e
CarlosSalega <carlosalfredosalega@gmail.com>
2026-03-18 22:39:32 -0300

    refactor(auth): update zod validation schema

--- src/features/auth/schemas/authSchema.ts
+++ src/features/auth/schemas/authSchema.ts
@@ -1,12 +1,14 @@
 import { z } from "zod";
 
 export const BaseAuthSchema = z.object({
+  name: z.string(),
   email: z.email({ error: "Invalid email address" }),
   password: z.string().min(8, { error: "Password must be at least 8 characters long" }),
   passwordConfirmation: z.string().min(1, { error: "Please confirm your password" }),
 });
 
 export const SignUpSchema = BaseAuthSchema.pick({
+  name: true,
   email: true,
   password: true,
   passwordConfirmation: true,
