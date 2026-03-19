67180d5915b5f1ed3544d0651b857da7d52d1431
CarlosSalega <carlosalfredosalega@gmail.com>
2026-03-18 22:36:43 -0300

    feat(db): add auth schemas and relations generated by better-auth

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,8 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import { relations } from "./relations";
+import * as schema from "./schemas";
 
-export const db = drizzle(process.env.DATABASE_URL!, {});
+export const db = drizzle(process.env.DATABASE_URL!, {
+  schema,
+  relations,
+});
